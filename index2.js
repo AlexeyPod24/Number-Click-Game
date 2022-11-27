7d41efa3ecbd30b614825236d01da2a7107d5533
Alexey Podkop <109368416+AlexeyPodkopaev@users.noreply.github.com>
2022-11-26 19:04:08 -0500

    Homework day

--- index2.js
+++ index2.js
@@ -1,4 +1,5 @@
 
+// ///////////////////////////////////////////////////////////////////////////////////////////////
 // // function palindrome(str) {
 // //    const joined = str.split('').reverse().join('')
 // //    if (str.toLowerCase() === joined.toLowerCase()) return true
@@ -11,18 +12,43 @@
 
 // console.log(palindrome('racecar'))
 
-let newArr = []
 
-function arrMultiply(arr) {
-for (let i = 0; i < arr.length; i++) {
-    if (arr[i] % 2 === 0) {
-     newArr += arr[i]
-     console.log(newArr[i])
-    }
-       
+
+// NO 2 v1 ////////////////////////////////////////////////////////////////////////////////////////////
+
+// let newArr = []
+// let total = []
+
+// function arrMultiply(arr) {
+// for (let i = 0; i < arr.length; i++) {
     
-}
-return newArr
-}
+//     if (arr[i] % 2 === 0) {
+//         arr[i] += newArr.push(arr[i])
+//     }   
+ 
+// }
 
-arrMultiply([1, 2, 4, 5, 6, 7, 8, 9, 10])
+// for (let i = 0; i < newArr.length; i++) {
+//     total.push (newArr[i] * 2)
+// }
+ 
+// return total
+
+// }
+
+// console.log(arrMultiply([1, 2, 4, 5, 6, 7, 8, 9, 10]))
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+// NO2 v2
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// arr.forEach(element => {
+//   if (element % 2 === 0) console.log(element * 2)
+
+// })
+
+
