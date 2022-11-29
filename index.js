fae3316b450cd063000ed60a7727a33f8779e2d9
Alexey Podkop <109368416+AlexeyPodkopaev@users.noreply.github.com>
2022-11-28 21:10:25 -0500

    Game Latest Version

--- index.js
+++ index.js
@@ -5,13 +5,20 @@
 // change background when clicking
 // reset button
 // 
+
+// const squaresCount = prompt('How many squares do you want?')
+
 let startBtn = document.querySelector('.start-btn')
 let resetBtn = document.querySelector('.reset-btn')
 let timer = document.querySelector('.time__countdown')
-const squares = [...document.querySelectorAll('.square')];
+const squares = document.querySelectorAll('.grid .square');
 let gameStarted = false;
 let counter = 60
 let timeInterval;
+
+
+
+let nums = []
 
 
 
@@ -27,13 +34,13 @@ startBtn.addEventListener('click', startGame)
 function startGame() {
 gameStarted = true;
 
-    if (gameStarted === true) {
-        for (let i = 0; i < squares.length; i++) {
-            squares[i].addEventListener('click', function() {
-                squares[i].style.backgroundColor = 'lightgreen'
-            }) 
-        } 
-    }
+    // if (gameStarted === true) {
+    //     for (let i = 0; i < squares.length; i++) {
+    //         squares[i].addEventListener('click', function() {
+    //             squares[i].style.backgroundColor = '#2A3166'
+    //         }) 
+    //     } 
+    // }
     randomNumber() 
 
    timeInterval = setInterval(function () {
@@ -42,6 +49,8 @@ gameStarted = true;
             timer.innerHTML = `Time left: ${counter}`
         }
     }, 1000)
+
+    
 }  
 
        
@@ -57,14 +66,37 @@ function getRandomColor() {
 
 
 function randomNumber() {
-  
+  const numArray = []
+    
+
+
+    for (let i = 1; i < 26; i++) {
+        numArray.push(i)
+
+    }
+    
+    numArray.sort(() => 0.5 - Math.random())
+
+
     for (let i = 0; i < squares.length; i++) {
         const random = Math.floor(Math.random() * 25)
-        squares[i].textContent = random
+    
+         squares[i].textContent = numArray[i]
+         if (numArray[i] === 1) squares[i].addEventListener('click', function() { 
+             squares[i].style.backgroundColor = 'red'
+             squares[i].classList.add('clicked')
+             
+             
+        })
+
+
+
         squares[i].style.fontSize = random + 15 + "px";
         squares[i].style.color = getRandomColor()
-        squares[i].style.backgroundColor = '#1F2937'
+        squares[i].style.backgroundColor = '#000000'
 }
+
+
 
 
 }
@@ -74,6 +106,11 @@ resetBtn.addEventListener('click', function() {
     counter = 60
     timer.innerHTML = `Time left: 60`
     gameStarted = false
+    squares.forEach((n) => {
+        n.textContent = "";
+        n.style.backgroundColor = "#000000";
+    })
+
     
 
 })
--- index2.js
+++ index2.js
@@ -1,42 +1,43 @@
 
 // ///////////////////////////////////////////////////////////////////////////////////////////////
-// // function palindrome(str) {
-// //    const joined = str.split('').reverse().join('')
-// //    if (str.toLowerCase() === joined.toLowerCase()) return true
-   
-// //    else false
+function palindrome(str) {
+  for (let i = 0; i < str.length / 2; i++) {
+    if (str[i] !== str[str.length - 1 - i]) {
+        return false
+    }
+    
+  } return true 
+}
 
-
-
-// // }
-
-// console.log(palindrome('racecar'))
+console.log(palindrome('abccba'))
 
 
 
 // NO 2 v1 ////////////////////////////////////////////////////////////////////////////////////////////
 
-// let newArr = []
-// let total = []
+let newArr = []
+let total = []
 
-// function arrMultiply(arr) {
-// for (let i = 0; i < arr.length; i++) {
+function arrMultiply(arr) {
+for (let i = 0; i < arr.length; i++) {
     
-//     if (arr[i] % 2 === 0) {
-//         arr[i] += newArr.push(arr[i])
-//     }   
+    if (arr[i] % 2 === 0) {
+        // arr[i] += newArr.push(arr[i])
+        arr[i] = arr[i] * 2
+    }   
  
-// }
+}
 
-// for (let i = 0; i < newArr.length; i++) {
-//     total.push (newArr[i] * 2)
-// }
+for (let i = 0; i < newArr.length; i++) {
+    total.push (newArr[i] * 2)
+}
  
-// return total
+return total
 
-// }
+}
 
-// console.log(arrMultiply([1, 2, 4, 5, 6, 7, 8, 9, 10]))
+console.log(arrMultiply([1, 2, 4, 5, 6, 7, 8, 9, 10]))
+// USE MAP FUNCTION
 
 
 
@@ -49,6 +50,13 @@
 // arr.forEach(element => {
 //   if (element % 2 === 0) console.log(element * 2)
 
+
+
 // })
+
+
+
+
+
 
 
